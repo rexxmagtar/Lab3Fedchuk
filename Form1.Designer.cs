2f9db0de1c977d8c5e3af14b48afda82d3937263
rexx.magtar <rexx.magtar@gmail.com>
2020-11-26 22:58:14 +0300

    lab done

---
+++ Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿
+namespace Fedchuk3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.InputFileText = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ConfirmButton = new System.Windows.Forms.Button();
+            this.outputTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // InputFileText
+            // 
+            this.InputFileText.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.InputFileText.Location = new System.Drawing.Point(131, 58);
+            this.InputFileText.Name = "InputFileText";
+            this.InputFileText.Size = new System.Drawing.Size(100, 34);
+            this.InputFileText.TabIndex = 0;
+            this.InputFileText.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(64, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Input file";
+            // 
+            // ConfirmButton
+            // 
+            this.ConfirmButton.Location = new System.Drawing.Point(237, 58);
+            this.ConfirmButton.Name = "ConfirmButton";
+            this.ConfirmButton.Size = new System.Drawing.Size(98, 34);
+            this.ConfirmButton.TabIndex = 2;
+            this.ConfirmButton.Text = "Confirm";
+            this.ConfirmButton.UseVisualStyleBackColor = true;
+            this.ConfirmButton.Click += new System.EventHandler(this.ConfirmButton_Click);
+            // 
+            // outputTextBox
+            // 
+            this.outputTextBox.Location = new System.Drawing.Point(358, 28);
+            this.outputTextBox.Multiline = true;
+            this.outputTextBox.Name = "outputTextBox";
+            this.outputTextBox.ReadOnly = true;
+            this.outputTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.outputTextBox.Size = new System.Drawing.Size(393, 384);
+            this.outputTextBox.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.outputTextBox);
+            this.Controls.Add(this.ConfirmButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.InputFileText);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox InputFileText;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button ConfirmButton;
+        private System.Windows.Forms.TextBox outputTextBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+using System.Windows.Forms;
+
+namespace Fedchuk3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private List<byte[]> DivideFile(string file)
+        {
+            List<byte[]> parts = new List<byte[]>();
+
+            long fileSize = new FileInfo(file).Length;
+
+            long partSize = (long)Math.Ceiling(1.0 * fileSize / 128);
+
+            long partCount = fileSize / partSize;
+
+            outputTextBox.Text = "";
+
+            outputTextBox.Text += "part size: " + partSize + Environment.NewLine;
+
+            outputTextBox.Text += "part count: " + partCount + Environment.NewLine;
+
+            using (BufferedStream reader = new BufferedStream(new FileStream(file, FileMode.OpenOrCreate)))
+            {
+                for (int i = 0; i < partCount - 1; i++)
+                {
+                    byte[] readBytes = new byte[partSize];
+
+                    reader.Read(readBytes, 0, (int)partSize);
+                    parts.Add(readBytes);
+
+                }
+
+                if (fileSize % partSize == 0)
+                {
+                    byte[] readBytes = new byte[partSize];
+
+                    reader.Read(readBytes, 0, (int)partSize);
+                    parts.Add(readBytes);
+                }
+                else
+                {
+                    long lastPartSize = (fileSize - partSize * (partCount - 1));
+
+                    byte[] readBytes = new byte[lastPartSize];
+
+                    reader.Read(readBytes, 0, (int)lastPartSize);
+                    parts.Add(readBytes);
+                }
+            }
+
+            return parts;
+        }
+
+        private static byte[] MD5(byte[] input)
+        {
+            return System.Security.Cryptography.MD5.Create().ComputeHash(input);
+        }
+
+        private async void ConfirmButton_Click(object sender, EventArgs e)
+        {
+            //var result = DivideFile(InputFileText.Text);
+
+            //for (int i = 0; i < result.Count; i++)
+            //{
+            //    outputTextBox.Text += Encoding.UTF8.GetString(MD5(result[i])) + Environment.NewLine;
+            //}
+
+            Stopwatch stopwatchAllTime = new Stopwatch();
+
+            Stopwatch stopwatchHashTime = new Stopwatch();
+
+            stopwatchAllTime.Start();
+
+            var buffer = new BufferBlock<byte[]>();
+
+            long timeRead = 0;
+
+            Produce(buffer, InputFileText.Text, ref timeRead);
+
+            stopwatchHashTime.Start();
+
+            var result = await ConsumeAsync(buffer);
+
+            stopwatchHashTime.Stop();
+
+            stopwatchAllTime.Stop();
+
+            long fileSize = 0;
+
+            try
+            {
+                fileSize = new FileInfo(InputFileText.Text).Length;
+            }
+            catch (Exception ex)
+            {
+                outputTextBox.Text += ex.Message;
+
+                return;
+            }
+
+            outputTextBox.Text += $"File size: {fileSize} bytes" +  Environment.NewLine;
+
+            try
+            {
+                outputTextBox.Text += "Time taken for all: " + stopwatchAllTime.ElapsedMilliseconds + " ms" + ". Speed:  " + (fileSize / stopwatchAllTime.ElapsedMilliseconds) + " byte/ms" + Environment.NewLine;
+                outputTextBox.Text += "Time taken for hash: " + stopwatchHashTime.ElapsedMilliseconds + " ms" + ". Speed:  " + (fileSize / stopwatchHashTime.ElapsedMilliseconds) + " byte/ms" + Environment.NewLine;
+                outputTextBox.Text += "Time taken for read: " + timeRead + " ms" + ". Speed:  " + (fileSize / timeRead) + " byte/ms" + Environment.NewLine;
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+            }
+
+            for (int i = 0; i < result.Count; i++)
+            {
+                outputTextBox.Text += $"{i}: " + GetHashFromBytes((result[i])) + Environment.NewLine;
+            }
+
+        }
+
+        private static string GetHashFromBytes(byte[] bytes)
+        {
+            string result = "";
+
+            foreach (var b in bytes)
+            {
+                result += b.ToString("x2");
+            }
+
+            return result;
+        }
+
+
+        void Produce(ITargetBlock<byte[]> target, string file, ref long timeTaken)
+        {
+
+            Stopwatch stopwatchReadTime = new Stopwatch();
+
+            stopwatchReadTime.Start();
+
+            List<byte[]> parts = new List<byte[]>();
+
+            long fileSize = 0;
+
+            try
+            {
+                fileSize = new FileInfo(file).Length;
+            }
+            catch (Exception e)
+            {
+                outputTextBox.Text = e.Message;
+
+                return;
+            }
+
+            long partSize = (long)Math.Ceiling(1.0 * fileSize / 128);
+
+            long partCount = fileSize / partSize;
+
+            outputTextBox.Text = "";
+
+            using (BufferedStream reader = new BufferedStream(new FileStream(file, FileMode.OpenOrCreate)))
+            {
+                for (int i = 0; i < partCount - 1; i++)
+                {
+                    byte[] readBytes = new byte[partSize];
+
+                    reader.Read(readBytes, 0, (int)partSize);
+                    target.Post(readBytes);
+
+                }
+
+                if (fileSize % partSize == 0)
+                {
+                    byte[] readBytes = new byte[partSize];
+
+                    reader.Read(readBytes, 0, (int)partSize);
+                    target.Post(readBytes);
+                }
+                else
+                {
+                    long lastPartSize = (fileSize - partSize * (partCount - 1));
+
+                    byte[] readBytes = new byte[lastPartSize];
+
+                    reader.Read(readBytes, 0, (int)lastPartSize);
+                    target.Post(readBytes);
+                }
+            }
+
+            target.Complete();
+
+            stopwatchReadTime.Stop();
+
+            timeTaken = stopwatchReadTime.ElapsedMilliseconds;
+        }
+
+        static async Task<List<byte[]>> ConsumeAsync(ISourceBlock<byte[]> source)
+        {
+            List<byte[]> result = new List<byte[]>();
+
+            while (await source.OutputAvailableAsync())
+            {
+                byte[] data = await source.ReceiveAsync();
+
+                result.Add(MD5(data));
+            }
+
+            return result;
+        }
+    }
+}
